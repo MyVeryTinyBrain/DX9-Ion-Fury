f930603d6c310726e2584d52dce3990f40488820
seob98 <yongsoub9809@gmail.com>
2021-10-03 19:53:23 +0900

    상담끝
    ㅇㅇ

---
+++ Engine/IonFuryEditor/Pickable.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Pickable : public Component
+{
+public:
+	OverrideComponentFunction(Awake);
+
+	OverrideComponentFunction(Update);
+
+	void Settings(const wstring& localPathMesh = L"../Resource/CubeUserMesh.mesh"
+		, const wstring& localPathTexture = L"../SharedResourced/Texture/Dev.png");
+
+	static Pickable* Pick(); //vec순환해서 
+
+	UserMeshRenderer* Get_Renderer() { return m_Renderer; }
+
+private:
+	GameObject*				m_ChildObject;
+	UserMeshRenderer*		m_Renderer;
+
+	static std::vector<Pickable*>	g_PickableVec;
+};
+
