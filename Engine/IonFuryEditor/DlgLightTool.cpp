ef6a54bc28e2bc1f00c95c090bc82afcfea33c83
seob98 <yongsoub9809@gmail.com>
2021-10-05 02:05:14 +0900

    변수1함수1함수1
    함수1만 제대로된위치에

--- Engine/IonFuryEditor/DlgLightTool.cpp
+++ Engine/IonFuryEditor/DlgLightTool.cpp
@@ -13,6 +13,7 @@ IMPLEMENT_DYNAMIC(DlgLightTool, CDialog)
 
 DlgLightTool::DlgLightTool(CWnd* pParent /*=nullptr*/)
 	: CDialog(IDD_DlgLightTool, pParent)
+	, ABCMEMBER(FALSE)
 {
 
 }
@@ -24,11 +25,20 @@ DlgLightTool::~DlgLightTool()
 void DlgLightTool::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	DDX_Check(pDX, ABC, ABCMEMBER);
 }
 
 
 BEGIN_MESSAGE_MAP(DlgLightTool, CDialog)
+	ON_BN_CLICKED(mian, &DlgLightTool::lighttooltouchtouchyeasbays)
 END_MESSAGE_MAP()
 
 
 // DlgLightTool 메시지 처리기
+
+
+void DlgLightTool::lighttooltouchtouchyeasbays()
+{
+	int i = 8;
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
