e034186752cb8891d84c94bad1d10abc243787c1
seob98 <yongsoub9809@gmail.com>
2021-10-22 18:17:12 +0900

    d
    d

--- Engine/IonFuryEditor/EditorEnum.h
+++ Engine/IonFuryEditor/EditorEnum.h
@@ -5,7 +5,7 @@ enum class Type { EventObject, Trigger, Map, TypeEnd };
 
 enum class EventType { BasicMutant, Mutant, CultistArcher, CultistGunner, Deacon, Drone, Liberator,
 	Spider, Skull, Warmech, Wendigo, Door, EventObjectEnd };	//추후 몬스터가 정해지면 수정
-enum class TriggerMethod { Touch, Button, Manual, End };
+enum class TriggerMethod { Touch, Button, Manual, CardCheck, End };
 
 enum class HandleObjectType { ItemBowAmmo, ItemChaingunAmmo, ItemLauncherAmmo,
 	ItemRevolverAmmo, ItemShotgunAmmo, ItemSMGAmmo, ItemHealthPack, End};
--- Engine/IonFuryEditor/Resource.h
+++ Engine/IonFuryEditor/Resource.h
@@ -102,6 +102,7 @@
 #define IDC_RotSlider2                  1052
 #define IDC_RADIO2                      1052
 #define IDC_RotSlider3                  1053
+#define IDC_RADIO1                      1053
 #define IDC_RADIO3                      1055
 #define IDC_RADIO4                      1056
 #define ID_32771                        32771
@@ -117,7 +118,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        317
 #define _APS_NEXT_COMMAND_VALUE         32777
-#define _APS_NEXT_CONTROL_VALUE         1053
+#define _APS_NEXT_CONTROL_VALUE         1054
 #define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
