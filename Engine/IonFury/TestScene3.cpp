2abdbcdf162733f8e3994b155b0289e474ae66ef
Seoseongyeon <87797199+Seoseongyeon@users.noreply.github.com>
2021-10-24 05:36:10 +0900

    123
    123

--- Engine/IonFury/TestScene3.cpp
+++ Engine/IonFury/TestScene3.cpp
@@ -24,27 +24,27 @@ void TestScene3::OnLoad(Scene* beforeScene)
 {
 	AddSkyBox();
 	//LightLoad::LightObjectLoadJson();
-	//TotalLoad::Load(L"../Data/Total/seongyeontest.txt");
-	//{   // Create test player
-	//	auto obj = CreateGameObject();
-	//	obj->transform->position = Vec3(0, 0, 0);
-	//	auto controller = obj->AddComponent<Player>();
-	//}
-
-	{   // Create directional light
+	TotalLoad::Load(L"../Data/Total/seongyeontest.txt");
+	{   // Create test player
 		auto obj = CreateGameObject();
-
-		auto renderer = obj->AddComponent<UserMeshRenderer>();
-		renderer->userMesh = Resource::FindAs<UserMesh>(BuiltInSphereUserMesh);
-		renderer->SetTexture(0, Resource::FindAs<Texture>(L"../SharedResource/Texture/Dev.png"));
-
-		obj->transform->position = Vec3(0, 5, 0);
-		obj->transform->forward = Quat::FromEuler(25, 0, 45) * Vec3::down();
-		auto light = obj->AddComponent<DirectionalLight>();
-		light->ambientFactor = 0.6f;
+		obj->transform->position = Vec3(0, 15, 15);
+		auto controller = obj->AddComponent<Player>();
 	}
-	AddMap();
-	AddMonster();
+
+	//{   // Create directional light
+	//	auto obj = CreateGameObject();
+
+	//	auto renderer = obj->AddComponent<UserMeshRenderer>();
+	//	renderer->userMesh = Resource::FindAs<UserMesh>(BuiltInSphereUserMesh);
+	//	renderer->SetTexture(0, Resource::FindAs<Texture>(L"../SharedResource/Texture/Dev.png"));
+
+	//	obj->transform->position = Vec3(0, 5, 0);
+	//	obj->transform->forward = Quat::FromEuler(25, 0, 45) * Vec3::down();
+	//	auto light = obj->AddComponent<DirectionalLight>();
+	//	light->ambientFactor = 0.6f;
+	//}
+	//AddMap();
+	//AddMonster();
 	//  MapLoad::LoadMap(L"../Data/Map/testmap.txt");
 
 
